51fdddc7bae2dc0b96845b981ab28c8878ab295a
lule moses <lulemoses67@gmail.com>
2022-02-03 23:49:13 +0300

    new theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,44 @@
-
 $(() => {
 
-    $('.logo').html(author.name);
     //button to toggle the menu on small screens
-    $('.btn-menu').on("click", () => $('#navBar').slideToggle());
+    $('.btn-menu').on("click", () => $('nav').slideToggle());
+
+    //Have a bug want that specify nav display it own childs with that class only 
+    if ($(window).width() < 768) {
+        $('.nav-item').on("click", () => {
+           $('nav-bars.').hide();
+        });
+    }
     
     //Navbar responsive when screen resizes
     $(window).on("resize", () => {
-        if ($(window).width() < 768 ) {
-            $('#navBar').hide();
-        } else{
-            $('#navBar').show();
-
-        }
+        if ($(window).width() > 768 )
+            $('nav').show();
+        else
+            $('nav').hide();
       });
-});
 
+
+    // animated texts
+    const animatedText = (elm,text) => {
+        let x = text ;
+        let y ='';
+        let i = 0; 
+        setInterval(() => {
+            if (i < x.length) {
+                $(elm).html(y);
+                y += x[i];
+                i++;
+            } else if (i >= x.length ) {
+                i = 0;
+                y = '';
+            }
+        }, 250);
+        console.log(x.length);
+    }
+    
+    animatedText('#lul','Hi , am Moses a Front-end Developer.');
+    //breadcrumbs
+   
+    
+});
