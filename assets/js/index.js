639855be4dbfe503c15f1e62761b657f4e8df472
lule moses <lulemoses67@gmail.com>
2022-02-22 12:47:26 +0300

    modal &  google fonts

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ $(() => {
     //Have a bug want that specify nav display it own childs with that class only 
     if ($(window).width() < 768) {
         $('.nav-item').on("click", () => {
-           $('nav-bars.').hide();
+           $('nav').hide();
         });
     }
     
@@ -30,15 +30,50 @@ $(() => {
                 y += x[i];
                 i++;
             } else if (i >= x.length ) {
-                i = 0;
-                y = "";
-            }
+                $(elm).html('scroll down <i class="fas fa-caret-down"></i>' );
+                setTimeout(function() { 
+                    i = 0;
+                    y = "";
+                }, 1000);
+            } 
         }, 250);
         console.log(x.length);
     }
     
     animatedText('#lul','A Front-end Developer.');
     //breadcrumbs
-    $(".hero").delay(1000).animate({ opacity: 1 }, 700);
-    
+    {
+        $(".hero").delay(1000).animate({ opacity: 1 }, 700);
+    }
+
+    {//modal
+        $('.toggle-modal').on("click", () => $('.modal').fadeToggle());
+    }
+
+    { //form
+        
+        $('#contactForm').on('submit', function( e ){ 
+            var form = $( this ), // this will resolve to the form submitted
+                action = form.attr( 'action' ),
+                  type = form.attr( 'method' ),
+                  data = {};
+         
+              // Make sure you use the 'name' field on the inputs you want to grab. 
+            form.find( '[name]' ).each( function( i , v ){
+               var input = $( this ), // resolves to current input element.
+                   name = input.attr( 'name' ),
+                   value = input.val();
+               data[name] = value;
+            });
+         
+           // Code which makes use of 'data'.
+            let hul = data;
+            $('.modal').fadeToggle();
+            alert('Thank You')
+            console.log(hul);
+            e.preventDefault();
+         });
+        
+        
+    }
 });
