a9d50e06e9edc03b05aac8c498a1993c8cb8ee9f
lule moses <lulemoses67@gmail.com>
2022-05-13 08:37:27 +0300

    javascript type modules failed

--- assets/js/Util.js
+++ assets/js/Util.js
@@ -1,12 +1,4 @@
 
-//A function to show a template taking the the data and element to put data and reusable 
-export const showTemplate=(a,b,c) => {
-    let source = $(a).html();
-    var template = Handlebars.compile(source);
-    var result = template(c);
-    $(b).html(result);
-}
-
 // animated texts
 export const animatedText = (elm,text) => {
     let x = text ;
