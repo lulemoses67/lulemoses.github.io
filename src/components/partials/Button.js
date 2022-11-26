487cbcd9a4253e4281c18ba099af9e94b818ca4d
Lule Moses <lulemoses67@gmail.com>
2022-11-26 20:09:06 +0300

    Updates

--- src/components/partials/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = (props) => {
-    return ( 
-        <>
-            <button onClick={props.func} className={props.btnColor}><i className={props.icon}></i> {props.name}</button>
-        </>
-     );
-}
- 
-export default Button;
